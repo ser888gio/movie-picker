b01070672e9238c6621dfbb73cc99dcbf7f94b52
nicas <nica.sergiu@seznam.cz>
2024-06-22 07:25:51 +0100

    Created a new web app for users to pick their movies

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Examples from './records.json'
+
+function App() {
+
+  return (
+    <>
+        {
+            //Check, if we have what to map. Mapping functions assigns to every element an attribute from json
+            Examples && Examples.map(example => {
+              return (
+                  <div className="movie-box" key= {example.id}>
+                    <h1 id = "title">{example.title}</h1>
+                    <p id = "rating">{example.vote_average}</p>
+                    <p id = "desc">{example.overview}</p>
+
+                    <div id = "genres">
+                      {example && example.genres.map( genre =>{
+                        return(
+                            <div key = {genre.id}> {genre.name}</div>
+                        )
+                      })}
+                    </div>
+                  </div>
+              )
+            })
+        }
+    </>
+  )
+}
+
+export default App
