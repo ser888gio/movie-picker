7a4ad0f5d317cb56f5da2313dcc33af7f5e3edc2
Odin-project-two <nica.sergiu@seznam.cz>
2024-06-23 09:07:12 +0200

    Fixed the API issue

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,8 @@
 import express from "express";
-import {post} from "axios";
-import fetch from "node-fetch";
-
 const app = express()
 app.use(express.json());
 const port = 5174
-const API_KEY = "f0e7d92d2294faa8867a03d09c327ec7"
+
 app.get('/', (req, res) => {
     res.send("Server is ready")
     }
@@ -15,21 +12,6 @@ app.get('/', (req, res) => {
 app.post('/send-data', async (req, res) =>{
     const genre = req.body.genre;
     const year = req.body.year;
-    console.log(post)
-
-    const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGU3ZDkyZDIyOTRmYWE4ODY3YTAzZDA5YzMyN2VjNyIsIm5iZiI6MTcxOTAzODEzNy44ODgzNzIsInN1YiI6IjY2NzNkOWU5NmYxYTFmN2Y0NThmMTg2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UH_ycR7sNcRXOW0vQN40SbWgKHJNrgm0O0lZ9ps3S44'
-        }
-    };
-
-    fetch(url, options)
-        .then(res => res.json())
-        .then(json => console.log(json))
-        .catch(err => console.error('error:' + err));
 
     res.send({message: `Genre ${genre} and year: ${year}`})
 })
